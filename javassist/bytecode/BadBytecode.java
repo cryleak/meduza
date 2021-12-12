5970a398dccc468a7ebdd3ab09c2f8c1cdf924fd
cryleak <58886961+cryleak@users.noreply.github.com>
2021-12-12 15:40:54 +0100

    Add files via upload

---
+++ javassist/bytecode/BadBytecode.java
@@ -0,0 +1,26 @@
+/*
+ * Decompiled with CFR 0.150.
+ */
+package javassist.bytecode;
+
+import javassist.bytecode.MethodInfo;
+
+public class BadBytecode
+extends Exception {
+    public BadBytecode(int opcode) {
+        super("bytecode " + opcode);
+    }
+
+    public BadBytecode(String msg) {
+        super(msg);
+    }
+
+    public BadBytecode(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public BadBytecode(MethodInfo minfo, Throwable cause) {
+        super(minfo.toString() + " in " + minfo.getConstPool().getClassName() + ": " + cause.getMessage(), cause);
+    }
+}
+
---
+++ javassist/bytecode/DuplicateMemberException.java
@@ -0,0 +1,14 @@
+/*
+ * Decompiled with CFR 0.150.
+ */
+package javassist.bytecode;
+
+import javassist.CannotCompileException;
+
+public class DuplicateMemberException
+extends CannotCompileException {
+    public DuplicateMemberException(String msg) {
+        super(msg);
+    }
+}
+
