d76786fa20ca4a9263c2aa3b840b877929f40859
cryleak <58886961+cryleak@users.noreply.github.com>
2021-12-12 15:42:33 +0100

    Add files via upload

---
+++ javassist/util/proxy/MethodFilter.java
@@ -0,0 +1,11 @@
+/*
+ * Decompiled with CFR 0.150.
+ */
+package javassist.util.proxy;
+
+import java.lang.reflect.Method;
+
+public interface MethodFilter {
+    public boolean isHandled(Method var1);
+}
+
---
+++ javassist/util/proxy/Proxy.java
@@ -0,0 +1,11 @@
+/*
+ * Decompiled with CFR 0.150.
+ */
+package javassist.util.proxy;
+
+import javassist.util.proxy.MethodHandler;
+
+public interface Proxy {
+    public void setHandler(MethodHandler var1);
+}
+
