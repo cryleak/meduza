f0d1543f2c4e6037ae8aee1a6746addd3dad805e
cryleak <58886961+cryleak@users.noreply.github.com>
2021-12-12 15:55:53 +0100

    Add files via upload

---
+++ org/yaml/snakeyaml/util/PlatformFeatureDetector.java
@@ -0,0 +1,16 @@
+/*
+ * Decompiled with CFR 0.150.
+ */
+package org.yaml.snakeyaml.util;
+
+public class PlatformFeatureDetector {
+    private Boolean isRunningOnAndroid = null;
+
+    public boolean isRunningOnAndroid() {
+        if (this.isRunningOnAndroid == null) {
+            this.isRunningOnAndroid = System.getProperty("java.runtime.name").startsWith("Android Runtime");
+        }
+        return this.isRunningOnAndroid;
+    }
+}
+
